5d7087767280d9596f4d2e5ef5c7fd5ce5d5b753
ArameAmiryn2007 <amiryana42@gmail.com>
2025-03-17 17:04:42 +0400

    edit app.js!

--- src/App.js
+++ src/App.js
@@ -5,33 +5,37 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Work from "./pages/Work";
 import Bloge from "./pages/Bloge";
+import NotFound from "./pages/NotFound"; // Create a NotFound component for 404 handling
+
 const router1 = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    children: [
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      
-      {
-        path: "/work",
-        element: <Work />,
-      },
-      
-      {
-        path: "/bloge",
-        element: <Bloge />,
-      },
-    ],
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/work",
+    element: <Work />,
+  },
+  {
+    path: "/bloge",
+    element: <Bloge />,
+  },
+  {
+    path: "*", // This route will catch all undefined routes
+    element: <NotFound />, // Custom 404 Page
   },
 ]);
+
 function App() {
   return <RouterProvider router={router1} />;
 }
+
 export default App;
