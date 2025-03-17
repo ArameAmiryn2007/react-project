98f24bcd47b708f90fa592875fc09ff66ab8b5fb
ArameAmiryn2007 <amiryana42@gmail.com>
2025-03-17 17:23:18 +0400

    edit app.js!

--- src/App.js
+++ src/App.js
@@ -5,39 +5,33 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Work from "./pages/Work";
 import Bloge from "./pages/Bloge";
-
-import ErrorPage from "./pages/ErrorPage"; // Create an ErrorPage component for handling errors
-
 const router1 = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    errorElement: <ErrorPage />, // Add error handling for this route
-  },
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />, // Error handling
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-    errorElement: <ErrorPage />, // Error handling
-  },
-  {
-    path: "/work",
-    element: <Work />,
-    errorElement: <ErrorPage />, // Error handling
-  },
-  {
-    path: "/bloge",
-    element: <Bloge />,
-    errorElement: <ErrorPage />, // Error handling
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+
+      {
+        path: "/work",
+        element: <Work />,
+      },
+
+      {
+        path: "/bloge",
+        element: <Bloge />,
+      },
+    ],
   },
 ]);
-
 function App() {
-  return <h1>Hello</h1>;
+  return <RouterProvider router={router1} />;
 }
-
 export default App;
