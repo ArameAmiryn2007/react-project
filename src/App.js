594248234907c8509da21783aacd6484544e0045
ArameAmiryn2007 <amiryana42@gmail.com>
2025-03-17 17:17:32 +0400

    edit app.js!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Work from "./pages/Work";
 import Bloge from "./pages/Bloge";
-import NotFound from "./pages/NotFound"; // For 404 errors
+
 import ErrorPage from "./pages/ErrorPage"; // Create an ErrorPage component for handling errors
 
 const router1 = createBrowserRouter([
@@ -34,15 +34,10 @@ const router1 = createBrowserRouter([
     element: <Bloge />,
     errorElement: <ErrorPage />, // Error handling
   },
-  {
-    // Catch-all route for non-existing pages
-    path: "*",
-    element: <NotFound />, // Display NotFound component for 404
-  },
 ]);
 
 function App() {
-  return <RouterProvider router={router1} />;
+  return <h1>Hello</h1>;
 }
 
 export default App;
